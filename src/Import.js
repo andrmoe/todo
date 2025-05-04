38f44077c642a5262fbab2e6ced0fdd0adda323d
andrmoe <mail@andreasmoe.com>
2025-05-04 19:34:16 +0200

    Added react router

---
+++ src/Import.js
@@ -0,0 +1,7 @@
+
+
+export function Import() {
+    return <>
+        <h1>Import from Remember the Milk</h1>
+    </>
+}
--- src/index.js
+++ src/index.js
@@ -2,14 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Import } from './Import';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route index element={<App/>}/>
+      <Route path="import" element={<Import/>}/> 
+    </Routes>
+  </BrowserRouter>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
