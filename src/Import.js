2e002e6c738d042d9da2a7a699bc789baabb1bf3
andrmoe <mail@andreasmoe.com>
2025-05-06 18:19:39 +0200

    Added file upload to import page

--- src/Import.js
+++ src/Import.js
@@ -1,7 +1,21 @@
-
+import { useState } from "react"
 
 export function Import() {
+    const [fileContent, setFileContent] = useState("")
+    const reader = new FileReader()
+    reader.onload = (e) => {
+        setFileContent(e.target.result)
+    }
     return <>
         <h1>Import from Remember the Milk</h1>
+        <input type="file" id="remjson" onChange={(event) => {
+            const selectedFile = event.target.files[0]
+            if (selectedFile) {
+                reader.readAsText(selectedFile)
+            }
+        }}/>
+        <button onClick={}>Import</button>
+        <div>{fileContent}</div>
+
     </>
 }
