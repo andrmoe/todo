769945985af71d17f3f1ee0adb260dfe34ea7488
andrmoe <mail@andreasmoe.com>
2025-10-24 19:35:27 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import { Import } from './Import';
 import { HashRouter, Route, Routes } from 'react-router-dom';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <HashRouter>
